e69ea6da60169eed6f04ef01d24138cbbea1b026
oyajibuki <oyajibuki@gmail.com>
2026-03-03 20:46:11 +0900

    Adjust height

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ if page == "lp":
     }
     </style>
     """, unsafe_allow_html=True)
-    components.html(lp_html, height=5800)
+    components.html(lp_html, height=5700)
     st.stop()
 
 # ── 成功ページ ──
